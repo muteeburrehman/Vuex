b68c51802d0042ae70c5b990415b4aa7c9d6b1cd
muteeburrehman <muteeb285@gmail.com>
2023-10-19 19:54:33 +0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,47 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <template>
+  <base-container title="Vuex">
+  <the-counter></the-counter>
+  <favourite-value></favourite-value>
+  <button @click="addOne">Add 10</button>
+  <change-counter></change-counter>
+  </base-container>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import BaseContainer from './components/BaseContainer.vue';
+import TheCounter from './components/TheCounter.vue';
+import ChangeCounter from './components/ChangeCounter.vue';
+import FavouriteValue from './components/FavouriteValue.vue';
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    BaseContainer,
+    TheCounter,
+    ChangeCounter,
+    FavouriteValue
+  },
+ 
+  methods: {
+    addOne(){
+      // this.$store.dispatch('increase',{value: 10});
+      this.$store.dispatch({
+        type: 'increase',
+        value: 10
+      })
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,51 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createStore } from "vuex";
 
-createApp(App).mount('#app')
+import App from "./App.vue";
+
+const store = createStore({
+  state() {
+    return {
+      counter: 0,
+    };
+  },
+  mutations: {
+    increment(state) {
+        state.counter = state.counter + 2;
+      
+  
+    },
+    increase(state, payload) {
+        state.counter = state.counter + payload.value
+    },
+  },
+  actions:{
+    increment(context) {
+        setTimeout(function(){
+           context.commit('increment')
+        },2000);
+    }
+  },
+  getters: {
+    finalCounter(state){
+        return  state.counter * 3;
+    },
+    normalizedCounter(_, getters){
+        // const finalCounter = state.counter *3;
+        const finalCounter = getters.finalCounter;
+        if(finalCounter<0){
+            return 0;
+        }
+        if(finalCounter> 100){
+            return 100;
+        }
+        return finalCounter;
+    }
+  }
+});
+
+const app = createApp(App);
+
+app.use(store);
+
+app.mount("#app");
